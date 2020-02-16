b5f4e168b3d9e32e328c917ba1a257aec5d467b2
Jonathan Levin <jon.levin25@gmail.com>
2020-02-16 08:48:17 +0200

    add init message (all excersizes for today) TODO: schedule this

--- app.js
+++ app.js
@@ -3,54 +3,76 @@ const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const Scheduler = require('./src/excersize_scheduler');
+const { getDayModel } = require('./src/sheets_parse');
+const { currTimeStr } = require('./src/helpers');
 
 const SlackMessage =  require('./src/slack/SlackMessage');
 const { sendSlackMessage, sendTestMessage } = require('./src/slack_wrapper');
 
+
 /* Partial flow used from: https://www.youtube.com/watch?v=8XBNz7cKvsc */
 
-const config = JSON.parse(fs.readFileSync('./config.json'));
+var config = JSON.parse(fs.readFileSync('./config.json'));
 console.log("Config loaded!");
 
-// Init
-const app = express();
-app.use(bodyParser.json());
 const scheduler = new Scheduler(config.google_sheet_id, config.google_sheet_range);
+
 scheduler.start(30);
 
-const sendSlackExcersize = (excersizeModel) => {
+// TODO: schedule this for every morning (cron?)
+sendSlackMorningMessage();
+
+scheduler.on('excersize', excersizeModel => {
+    sendSlackExcersize(excersizeModel)
+})
+
+//postMessage(config.webhook_url, slackBody);
+// sendTestMessage('TEST!');
+
+function sendSlackExcersize (excersizeModel) {
     const {time, excersize} = excersizeModel;
     let slackMessage = new SlackMessage(`ITS TIME FOR ${excersize} MAGGOTS!`);
     slackMessage.addAttachment(`*${excersize}* [${time}]`);
     sendSlackMessage(slackMessage);
 }
 
-scheduler.on('excersize', excersizeModel => {
-    sendSlackExcersize(excersizeModel)
-})
+async function sendSlackMorningMessage() {
+    try {
+        const todayExcersizes = await getDayModel(config.google_sheet_id, config.google_sheet_range);
+        const slackMessage = new SlackMessage(
+            `Morning ladies! The time is ${currTimeStr()}. Here are your excersizes for today`);
 
+        todayExcersizes.forEach(excersizeModel => {  
+            const { time, excersize } = excersizeModel;
+            slackMessage.addAttachment(`*${time}*\t${excersize}`);
+        });
+
+        sendSlackMessage(slackMessage);
+    } catch (err) { console.error(err)}
+}
 
 // --- UNUSED --- 
-app.get('/', (req, res) => {
-    console.log("Home hit!");
-    res.end("HIT!");
-});
+function InitExpressApp(){
+    const app = express();
+    app.use(bodyParser.json());
 
-// Receive mentions from slack - currently unused
-app.post(config.route, (req, res) => {
-    console.log(`request: ${req.body}`)
-
-    // Slack route verification
-    if (req.body.type == 'url_verification'){
-        let challenge = req.body.challenge;
-
-        console.log(`responding with ${challenge}`)
-        res.end(challenge);
-    }
-});
-
-//postMessage(config.webhook_url, slackBody);
-
-app.listen(config.port);
-
-// sendTestMessage('TEST!');
+    app.get('/', (req, res) => {
+        console.log("Home hit!");
+        res.end("HIT!");
+    });
+    
+    // Receive mentions from slack - currently unused
+    app.post(config.route, (req, res) => {
+        console.log(`request: ${req.body}`)
+    
+        // Slack route verification
+        if (req.body.type == 'url_verification'){
+            let challenge = req.body.challenge;
+    
+            console.log(`responding with ${challenge}`)
+            res.end(challenge);
+        }
+    });
+    
+    app.listen(config.port);
+}
